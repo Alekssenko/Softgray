39f8f773614ba1b012963d4956605540ebdae277
Alekssenko <sasha18081960@gmail.com>
2019-02-02 10:41:46 +0200

    02.02.19 start

--- js/script.js
+++ js/script.js
@@ -3,29 +3,10 @@
  */
 $(document).ready(function () {
 
-    //======================================вариант слева=============================
-    // $('.header .nav-menu').removeClass('passive');
-    // $('.burger').click(function (e) {
-    //     e.stopPropagation();
-    //     $('.header .nav-menu').addClass('active');
-    //     $('.header .nav-menu').removeClass('passive');
-    // });
-    // $('.main-area').click(function (e) {
-    //     e.stopPropagation();
-    //     if($(window).width <770) {
-    //         $('.header .nav-menu').removeClass('active');
-    //         $('.header .nav-menu').addClass('passive');
-    //     }
-    //     $('.header .nav-menu').removeClass('passive');
-    //     $('.header .nav-menu').removeClass('active');
-    // });
-
-//    ===========================ВАРИАНТ СПРАВА====================================
     $('.header .nav-menu').removeClass('passive');
     $('.burger').click(function (e) {
         e.stopPropagation();
         $('.header .nav-menu').toggleClass('active');
-        // $('.header .nav-menu').removeClass('passive');
     });
     $('.main-area').click(function (e) {
         e.stopPropagation();
@@ -36,13 +17,5 @@ $(document).ready(function () {
         $('.header .nav-menu').removeClass('passive');
         $('.header .nav-menu').removeClass('active');
     });
-
-
-
-
-
-
-
-
 });
 
